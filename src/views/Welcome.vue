c2eee1c803f3f2f9c63ca2725b2527f547c29031
rabgonzalez <rubalba.rag@gmail.com>
2026-02-10 18:33:21 +0000

    styles: tailwind styles and fonts changed

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="bg-primary">
-        <h1 class="text-xl">Welcome to MoviesX</h1>
+        <h1 class="text-5xl">Welcome to MoviesX</h1>
         <p>Your one-stop solution for all movie-related information.</p>
     </div>
 </template>
