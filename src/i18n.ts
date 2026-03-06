06068b04623b7f8dacfbf1a61e3d30f8ec71cd11
rabgonzalez <rubalba.rag@gmail.com>
2026-03-06 16:55:51 +0000

    tranlations: french and deutch languages added

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,12 +1,14 @@
 import { createI18n } from 'vue-i18n'
 import en from './locales/en.json'
 import es from './locales/es.json'
+import fr from './locales/fr.json'
+import de from './locales/de.json'
 
 const i18n = createI18n({
   legacy: false,
   locale: localStorage.getItem('locale') || 'en',
   fallbackLocale: 'en',
-  messages: { en, es }
+  messages: { en, es, fr, de }
 })
 
 export default i18n
