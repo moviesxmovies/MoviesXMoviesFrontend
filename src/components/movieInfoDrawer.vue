be16385507a83f8b74d1affce8b2251074c89bbb
rabgonzalez <rubalba.rag@gmail.com>
2026-04-10 16:59:09 +0100

    feat: drawer for movie more info

---
+++ src/components/movieInfoDrawer.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { Movie } from "@/types";
+import { Drawer } from "primevue";
+
+const props = defineProps<{
+  movie: Movie;
+}>();
+
+const visible = defineModel<boolean>("visible", { default: false });
+</script>
+
+<template>
+  <Drawer
+    v-model:visible="visible"
+    :header="movie.title"
+    :modal="false"
+    :dismissable="false"
+    position="right"
+    class="!w-full md:!w-[40rem]"
+  >
+    <p>
+      {{ movie.synopsis }}
+    </p>
+  </Drawer>
+</template>
