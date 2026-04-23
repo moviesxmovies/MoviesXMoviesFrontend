efb65a1a29d649eeaca9eca4fbbf1d35e2d994f9
rabgonzalez <rubalba.rag@gmail.com>
2026-04-23 10:20:46 +0100

    tests: some issues fixed

--- src/components/searchStarsComponent.vue
+++ src/components/searchStarsComponent.vue
@@ -1,12 +1,14 @@
 <script lang="ts" setup>
 import { Checkbox } from "primevue";
 import { ref, watch } from "vue";
+import { useI18n } from "vue-i18n";
 import { useRoute } from "vue-router";
 
 const selectedStars = ref<string[]>([]);
 const route = useRoute();
 const STARS = ["1", "2", "3", "4", "5"];
 const emit = defineEmits(["filterStars"]);
+const { t } = useI18n();
 
 watch(
   () => route.query.stars,
@@ -26,11 +28,11 @@ watch(
   <div v-for="star of STARS" :key="star" class="flex items-center gap-2">
     <Checkbox
       v-model="selectedStars"
-      :inputId="star"
+      :inputId="'star-' + star"
       name="stars"
       :value="star"
       @change="emit('filterStars', selectedStars)"
     />
-    <label :for="star">{{ star }}</label>
+    <label :for="star">{{ t("components.searchStar.star", [star]) }} </label>
   </div>
 </template>
