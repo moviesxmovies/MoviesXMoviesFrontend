bb5184a5a6a838f5dac7e6e57f139ef696e4e41a
rabgonzalez <rubalba.rag@gmail.com>
2026-05-09 11:14:29 +0100

    tests: old test refactored

--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -183,7 +183,7 @@ export const userSearching = async (
     const response = await api.get(`/users/searching/`, {
       params: {
         ...data,
-        limit: 5,
+        limit,
         search_query: data.name || undefined,
       },
     });
